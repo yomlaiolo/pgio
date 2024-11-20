fc8fa2339f9ff89174658fba941ae7829d400bd5
Yom <tom.laiolo@epitech.eu>
2024-11-20 13:54:32 -0800

    Finished tut14

---
+++ file
@@ -0,0 +1,4 @@
+111,Envelopes,100 letter size,11.50
+112,Post-it #1,3" by 3" 1000 sheets,9.90
+113,Post-it #2,2" by 2" 1000 sheets,7.90
+114,Post-it #3,4" by 6" 1000 sheets,13.90
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const multer = require("multer");
+const upload = multer();
+
 // Set up EJS
 app.set("view engine", "ejs");
 
@@ -12,7 +15,7 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.get("/", (req, res) => {
-  const sql = "SELECT * FROM CACA ORDER BY PROD_ID";
+  const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
   pool.query(sql, [], (err, result) => {
     let message = "";
     let model = {};
@@ -26,6 +29,58 @@ app.get("/", (req, res) => {
       message: message,
       model: model,
     });
+  });
+});
+
+app.get("/input", (req, res) => {
+  res.render("input");
+});
+app.post("/input", upload.single("filename"), (req, res) => {
+  if (!req.file || Object.keys(req.file).length === 0) {
+    let message = "Error: Import file not uploaded";
+    return res.send(message);
+  }
+  //Read file line by line, inserting records
+  const buffer = req.file.buffer;
+  const lines = buffer.toString().split(/\r?\n/);
+  lines.forEach((line) => {
+    //console.log(line);
+    let product = line.split(",");
+    //console.log(product);
+    const sql =
+      "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)";
+    pool.query(sql, product, (err, result) => {
+      if (err) {
+        console.log(`Insert Error. Error message: ${err.message}`);
+      } else {
+        console.log(`Inserted successfully`);
+      }
+    });
+  });
+  let message = `Processing Complete - Processed ${lines.length} records`;
+  res.send(message);
+});
+
+app.get("/output", (req, res) => {
+  let message = "";
+  res.render("output", { message: message });
+});
+app.post("/output", (req, res) => {
+  const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+  pool.query(sql, [], (err, result) => {
+    let message = "";
+    if (err) {
+      message = `Error - ${err.message}`;
+      res.render("output", { message: message });
+    } else {
+      let output = "";
+      result.rows.forEach((product) => {
+        output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+      });
+      res.header("Content-Type", "text/csv");
+      res.attachment("export.csv");
+      return res.send(output);
+    }
   });
 });
 
